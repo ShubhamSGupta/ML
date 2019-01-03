d764cc585a5d5d7e7f18f8b88b40a9f28ee89670
Shubham Gupta <40262284+ShubhamSGupta@users.noreply.github.com>
2019-01-03 15:23:43 +0530

    Add files via upload

---
+++ Performance Metrics/Regression Metrics/mae_regression.py
@@ -0,0 +1,16 @@
+# Cross Validation Regression MAE
+from pandas import read_csv
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LinearRegression
+from sklearn.datasets import load_boston
+array = load_boston().data
+print(array.shape)
+X = array[:, 0:12]
+Y = array[:, 12]
+kfold = KFold(n_splits=10, random_state=7)
+model = LinearRegression()
+scoring = 'neg_mean_absolute_error'
+results = cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+print("MAE: {}({})".format(results.mean(), results.std()))
+
