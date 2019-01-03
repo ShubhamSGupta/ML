417c35611b15ab823783db2f44d1624704f94239
Shubham Gupta <40262284+ShubhamSGupta@users.noreply.github.com>
2019-01-03 15:22:42 +0530

    Add files via upload

---
+++ Performance Metrics/CLassification Metrics/classification_report.py
@@ -0,0 +1,22 @@
+# Cross Validation Classification Report
+from pandas import read_csv
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+filename = 'data/breastCancer.data.csv'
+names = ['Id', ' Clump Thickness', 'cell size', 'cell shape', 'Marginal Adhesion', 'Single Epithelial Cell Size ',
+         'Bare_Nuclei', 'Bland Chromatin ', 'Normal Nucleoli', 'Mitoses', 'Class']
+dataframe = read_csv(filename, names=names)
+dataframe = dataframe[dataframe.Bare_Nuclei != '?']
+array = dataframe.values
+X = array[:, 1:10]
+Y = array[:, 10]
+Y = Y.astype('int')
+test_size = 0.33
+seed = 7
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
+model = LogisticRegression()
+model.fit(X_train, Y_train)
+predicted = model.predict(X_test)
+report = classification_report(Y_test, predicted)
+print(report)
