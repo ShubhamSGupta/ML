dbad4a16ee90d44af3cecd31f157ca6422a04c72
Shubham Gupta <40262284+ShubhamSGupta@users.noreply.github.com>
2019-01-03 15:27:20 +0530

    Add files via upload

---
+++ Spot Check Classification/knearest_neighbor.py
@@ -0,0 +1,18 @@
+from pandas import read_csv
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+filename = 'data/breastCancer.data.csv'
+names = ['Id', ' Clump Thickness', 'cell size', 'cell shape', 'Marginal Adhesion', 'Single Epithelial Cell Size ',
+         'Bare_Nuclei', 'Bland Chromatin ', 'Normal Nucleoli', 'Mitoses', 'Class']
+dataframe = read_csv(filename, names=names)
+dataframe = dataframe[dataframe.Bare_Nuclei != '?']
+array = dataframe.values
+X = array[:,1:10]
+Y = array[:,10]
+Y = Y.astype('int')
+num_folds = 10
+kfold = KFold(n_splits=num_folds, random_state=7)
+model = KNeighborsClassifier()
+results = cross_val_score(model, X, Y, cv=kfold)
+print(results.mean())
---
+++ Spot Check Classification/naive_bayes.py
@@ -0,0 +1,18 @@
+# Gaussian Naive Bayes Classification
+from pandas import read_csv
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.naive_bayes import GaussianNB
+filename = 'data/breastCancer.data.csv'
+names = ['Id', ' Clump Thickness', 'cell size', 'cell shape', 'Marginal Adhesion', 'Single Epithelial Cell Size ',
+         'Bare_Nuclei', 'Bland Chromatin ', 'Normal Nucleoli', 'Mitoses', 'Class']
+dataframe = read_csv(filename, names=names)
+dataframe = dataframe[dataframe.Bare_Nuclei != '?']
+array = dataframe.values
+X = array[:,1:10]
+Y = array[:,10]
+Y = Y.astype('int')
+kfold = KFold(n_splits=10, random_state=7)
+model = GaussianNB()
+results = cross_val_score(model, X, Y, cv=kfold)
+print(results.mean())
