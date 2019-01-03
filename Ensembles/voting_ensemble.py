d2f5b4f60ee864227599ac2a91160b95bc611fc8
Shubham Gupta <40262284+ShubhamSGupta@users.noreply.github.com>
2019-01-03 15:38:27 +0530

    Add files via upload

---
+++ Ensembles/voting_ensemble.py
@@ -0,0 +1,30 @@
+# Voting Ensemble for Classification
+from pandas import read_csv
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import VotingClassifier
+filename = 'data/breastCancer.data.csv'
+names = ['Id', ' Clump Thickness', 'cell size', 'cell shape', 'Marginal Adhesion', 'Single Epithelial Cell Size ',
+         'Bare_Nuclei', 'Bland Chromatin ', 'Normal Nucleoli', 'Mitoses', 'Class']
+dataframe = read_csv(filename, names=names)
+dataframe = dataframe[dataframe.Bare_Nuclei != '?']
+array = dataframe.values
+X = array[:,1:10]
+Y = array[:,10]
+Y = Y.astype('int')
+kfold = KFold(n_splits=10, random_state=7)
+# create the sub models
+estimators = []
+model1 = LogisticRegression()
+estimators.append(('logistic', model1))
+model2 = DecisionTreeClassifier()
+estimators.append(('cart', model2))
+model3 = SVC()
+estimators.append(('svm', model3))
+# create the ensemble model
+ensemble = VotingClassifier(estimators)
+results = cross_val_score(ensemble, X, Y, cv=kfold)
+print(results.mean())
