aa80894ff9d1c52fee60cddf9dac378f51219a3d
Shubham Gupta <40262284+ShubhamSGupta@users.noreply.github.com>
2019-01-04 10:09:49 +0530

    Add files via upload

---
+++ Data processing/binarizer.py
@@ -0,0 +1,18 @@
+# binarization
+from sklearn.preprocessing import Binarizer
+from pandas import read_csv
+from numpy import set_printoptions
+filename = 'data/breastCancer.data.csv'
+names = ['Id', ' Clump Thickness', 'cell size', 'cell shape', 'Marginal Adhesion', 'Single Epithelial Cell Size ',
+         'Bare_Nuclei', 'Bland Chromatin ', 'Normal Nucleoli', 'Mitoses', 'Class']
+dataframe = read_csv(filename, names=names)
+dataframe = dataframe[dataframe.Bare_Nuclei != '?']
+array = dataframe.values
+# separate array into input and output components
+X = array[:,1:10]
+Y = array[:,10]
+binarizer = Binarizer(threshold=0.0).fit(X)
+binaryX = binarizer.transform(X)
+# summarize transformed data
+set_printoptions(precision=3)
+print(binaryX[0:7,:])
